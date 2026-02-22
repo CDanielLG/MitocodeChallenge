54cdbeccef0820c1cfcc16b84de7919b6beffae9
Daniel <hikivel139@gmail.com>
2026-02-22 02:53:44 -0600

    Remove example and script files

--- update_enrollment_30.ps1
+++
@@ -1,3 +0,0 @@
-curl.exe -X PUT "http://localhost:9494/v1/enrollments/30" `
-     -H "Content-Type: application/json" `
-     -d '{ \"idEnrollmentRegister\": 30, \"enrollmentDate\": \"2026-02-21T10:00:00\", \"student\": { \"idStudent\": 1 }, \"status\": true, \"enrollmentDetail\": [ { \"course\": { \"idCourse\": 1 }, \"classroom\": \"A101\" } ] }'
